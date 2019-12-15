2e66aef5103fe0f71a2b58aec610b94f22ca0280
Professor-Oz <50522753+Professor-Oz@users.noreply.github.com>
2019-12-15 17:58:42 +0530

    Update chat.ts

--- server/chat.ts
+++ server/chat.ts
@@ -374,7 +374,7 @@ export class CommandContext extends MessageContext {
 
 		// Output the message
 
-		if (message && message !== true && typeof message.then !== 'function') {
+		/*if (message && message !== true && typeof message.then !== 'function') {
 			if (this.pmTarget) {
 				Chat.sendPM(message, this.user, this.pmTarget);
 			} else {
@@ -387,6 +387,50 @@ export class CommandContext extends MessageContext {
 
 		this.update();
 
+		return message;
+	}*/
+        if (message && message !== true && typeof message.then !== 'function') {
+			if (this.pmTarget) {
+				Chat.sendPM(message, this.user, this.pmTarget);
+			} else {
+				let emoticons = Server.parseEmoticons(message);
+				if (emoticons && !this.room.disableEmoticons) {
+					if (Users.ShadowBan.checkBanned(this.user)) {
+						Users.ShadowBan.addMessage(this.user, "To " + this.room.id, message);
+						if (!Server.ignoreEmotes[this.user.userid]) this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|/html ' + emoticons);
+						if (Server.ignoreEmotes[this.user.userid]) this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
+						this.room.update();
+						return false;
+					}
+					for (let u in this.room.users) {
+						let curUser = Users(u);
+						if (!curUser || !curUser.connected) continue;
+						if (Server.ignoreEmotes[curUser.userid]) {
+							curUser.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
+							continue;
+						}
+						curUser.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|/html ' + emoticons);
+					}
+					this.room.log.log.push((this.room.type === 'chat' ? (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
+					this.room.lastUpdate = this.room.log.length;
+					this.room.messageCount++;
+				} else {
+					if (Users.ShadowBan.checkBanned(this.user)) {
+						Users.ShadowBan.addMessage(this.user, "To " + this.room.id, message);
+						this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
+					} else {
+						this.room.add((this.room.type === 'chat' ? (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
+						this.room.messageCount++;
+					}
+				}
+				//this.room.add(`|c|${this.user.getIdentity(this.room.id)}|${message}`);
+			}
+		}
+		if (this.room && this.room.game && this.room.game.onLogMessage) {
+			this.room.game.onLogMessage(message, this.user);
+		}
+		if (this.user.registered && giveExp) Server.ExpControl.addExp(this.user.userid, this.room, 1);
+		this.update();
 		return message;
 	}
 
@@ -626,6 +670,9 @@ export class CommandContext extends MessageContext {
 	sendReplyBox(htmlContent: string) {
 		this.sendReply(`|html|<div class="infobox">${htmlContent}</div>`);
 	}
+        sendRB(html) {
+      this.sendReply(`|html|${html}`);
+        }
 	popupReply(message: string) {
 		this.connection.popup(message);
 	}
